bb63ae7b67b6b253b6c94bf8337ae8cd1d957484
LukasHuth <60547284+LukasHuth@users.noreply.github.com>
2026-02-24 23:15:58 +0100

    Add documentation

--- src/diff/mod.rs
+++ src/diff/mod.rs
@@ -1,3 +1,3 @@
 use std::path::PathBuf;
 
-pub(crate) fn diff(base: PathBuf, source: PathBuf, fix: bool, output: Option<PathBuf>) {}
+pub(crate) fn diff(base: PathBuf, source: PathBuf, recursive: bool, fix: bool, output: Option<PathBuf>) {}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,53 @@
+#![deny(missing_docs)]
+//! This crate provides functionality to compare JSON files
 use std::path::PathBuf;
 
 use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about)]
-enum Options {
+/// Defines the command line arguments that can be used with this program
+pub enum Options {
     #[command(alias = "v")]
+    /// Checks whether the keys in the source file/directory align with the base file
     Verify {
+        /// This file will be used as base template for every source file
         base: PathBuf,
+        /// This can be a file or directory. It will be checked against the base
         source: PathBuf,
+        /// Wether to explore the specified source directory recursivly or not
+        /// This has no effect when source is a file
         #[arg(long, short, default_value_t = false)]
         recursive: bool,
     },
+    /// Checks whether the used templates in the source file/directory align with the base file
     #[command(alias = "vt")]
     VerifyTemplates {
+        /// This file will be used as base template for every source file
         base: PathBuf,
+        /// This can be a file or directory. It will be checked against the base
         source: PathBuf,
+        /// Wether to explore the specified source directory recursivly or not
+        /// This has no effect when source is a file
         #[arg(long, short, default_value_t = false)]
         recursive: bool,
     },
+    /// This will find all differences between base and source
     #[command(alias = "d")]
     Diff {
+        /// This file will be used as base template for every source file
         base: PathBuf,
+        /// This can be a file or directory. It will be checked against the base
         source: PathBuf,
+        /// Wether to explore the specified source directory recursivly or not
+        /// This has no effect when source is a file
+        #[arg(long, short, default_value_t = false)]
+        recursive: bool,
+        /// When this flag is active the source will be extended/trimmed based on the base.
+        /// If not supplied, a report will be printed, what is more or missing based on the base.
         #[arg(long, short, default_value_t = false)]
         fix: bool,
+        /// When supplied, the output will be written to this file instead of stdout.
         #[arg(long, short)]
         output: Option<PathBuf>,
     },
@@ -37,7 +60,8 @@ fn main() {
             source,
             fix,
             output,
-        } => diff::diff(base, source, fix, output),
+            recursive,
+        } => diff::diff(base, source, recursive, fix, output),
         Options::Verify {
             base,
             source,
